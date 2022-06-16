0094833e8df35334211d881d2cd1374d603ea397
gabriel <gabimtme@gmail.com>
2022-06-16 23:31:35 +0200

    feat: export interfaces from index

--- src/StlViewer/index.ts
+++ src/StlViewer/index.ts
@@ -1,2 +1,4 @@
+export {ModelDimensions} from "./SceneElements/Model3D";
 export {default as StlViewer} from "./StlViewer"
 export type {StlViewerProps} from "./StlViewer"
+export type {ModelProps, ModelRef, FloorProps} from "./SceneSetup"
