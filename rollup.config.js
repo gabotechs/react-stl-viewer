3a6db2609787286f960faddbf1d4cd33ea6dade7
Automated <Automated>
2022-10-30 18:17:35 +0100

    chore: bump react three fiber version

--- rollup.config.js
+++ rollup.config.js
@@ -15,12 +15,10 @@ export default [
             {
                 file: packageJson.main,
                 format: "cjs",
-                sourcemap: true,
             },
             {
                 file: packageJson.module,
                 format: "esm",
-                sourcemap: true,
             },
         ],
         plugins: [
